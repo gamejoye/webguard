bfa25758b669061985df48b67def0c963845c114
gamejoye <gamejoye@gmail.com>
2024-12-10 18:03:46 +0800

    feat(core): breadcrumb用户行为记录类

---
+++ packages/core/src/breadcrumb.ts
@@ -0,0 +1,45 @@
+import { BreadcrumbConfig, IBreadcrumbData } from "@web-guard/types";
+import { LinkedList } from "@web-guard/utils";
+
+export class Breadcrumb {
+  maxBreadcrumbs!: number;
+  beforePushBreadcrumb?: (breadcrumb: IBreadcrumbData) => IBreadcrumbData | null;
+  stack: LinkedList<IBreadcrumbData>;
+  constructor(config: BreadcrumbConfig = {}) {
+    this.bindConfig(config);
+    this.stack = new LinkedList<IBreadcrumbData>();
+  }
+
+  push(breadcrumb: IBreadcrumbData): IBreadcrumbData | null {
+    if (this.beforePushBreadcrumb) {
+      const result = this.beforePushBreadcrumb(breadcrumb);
+      if (!result) {
+        return null;
+      }
+      breadcrumb = result;
+    }
+    if (this.stack.getSize() >= this.maxBreadcrumbs) {
+      this.stack.shift();
+    }
+    this.stack.push(breadcrumb);
+    return breadcrumb;
+  }
+
+  clear(): void {
+    while (this.stack.getSize()) this.stack.shift();
+  }
+
+  getStack(): IBreadcrumbData[] {
+    return this.stack.toArray();
+  }
+
+  bindConfig(config: BreadcrumbConfig): void {
+    this.maxBreadcrumbs = config.maxBreadcrumbs ?? 30;
+    this.beforePushBreadcrumb = config.beforePushBreadcrumb;
+  }
+}
+
+const breadcrumb = new Breadcrumb();
+export {
+  breadcrumb,
+}
