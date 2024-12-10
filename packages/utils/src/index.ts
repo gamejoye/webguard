39ecae5467830a1c8b2cbf335e068261913c465a
gamejoye <gamejoye@gmail.com>
2024-12-10 18:01:18 +0800

    feat(utils): EventType BreadcrumbType BreadcrumbLevel转换

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,3 +2,4 @@ export * from './emitter';
 export * from './uuid';
 export * from './global';
 export * from './linked-list';
+export * from './transform';
---
+++ packages/utils/src/transform.ts
@@ -0,0 +1,27 @@
+import { EventTypes, BreadcrumbTypes, BreadcrumbLevel } from '@web-guard/common';
+
+/**
+ * 将 EventTypes 转换为对应的 BreadcrumbTypes
+ */
+export function eventTypeToBreadcrumbType(type: EventTypes): BreadcrumbTypes {
+  const map: Record<EventTypes, BreadcrumbTypes> = {
+    [EventTypes.ERROR]: BreadcrumbTypes.CODE_ERROR,
+    [EventTypes.UNHANDLEDREJECTION]: BreadcrumbTypes.CODE_ERROR,
+  };
+  return map[type];
+}
+
+/**
+ * 将 BreadcrumbTypes 转换为对应的 BreadcrumbLevel
+ */
+export function breadcrumbTypeToBreadcrumbLevel(type: BreadcrumbTypes): BreadcrumbLevel {
+  const map: Record<BreadcrumbTypes, BreadcrumbLevel> = {
+    [BreadcrumbTypes.HTTP]: BreadcrumbLevel.INFO,
+    [BreadcrumbTypes.CLICK]: BreadcrumbLevel.INFO,
+    [BreadcrumbTypes.ROUTE]: BreadcrumbLevel.INFO,
+    [BreadcrumbTypes.CONSOLE]: BreadcrumbLevel.DEBUG,
+    [BreadcrumbTypes.CODE_ERROR]: BreadcrumbLevel.ERROR,
+    [BreadcrumbTypes.CUSTOM]: BreadcrumbLevel.INFO,
+  };
+  return map[type];
+} 
