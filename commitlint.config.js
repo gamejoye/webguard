b9eac4f6864b4579ad0bf044ffa266254fc58b36
gamejoye <gamejoye@gmail.com>
2024-12-11 01:09:24 +0800

    chore: 关闭commit-message subject大小写检查

--- commitlint.config.js
+++ commitlint.config.js
@@ -28,7 +28,7 @@ const config = {
     'scope-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-case': [0],
     'header-max-length': [2, 'always', 72],
   },
 };
