4b238a87f33a8126f51aaba2c0a6ce5f18855f49
gamejoye <gamejoye@gmail.com>
2024-12-10 18:02:26 +0800

    build(core): core配置

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  root: '__tests__',
+  resolve: {
+    alias: {
+      '@web-guard/core': path.resolve(__dirname, './src'),
+      '@web-guard/utils': path.resolve(__dirname, '../utils/src'),
+      '@web-guard/common': path.resolve(__dirname, '../common/src'),
+      '@web-guard/types': path.resolve(__dirname, '../types/src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+})
