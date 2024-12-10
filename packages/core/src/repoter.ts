8aa36a831fe3ef48374f38372fd02958df8e5d80
gamejoye <gamejoye@gmail.com>
2024-12-10 18:03:14 +0800

    feat(core): monitor-reporter上报类

---
+++ packages/core/src/repoter.ts
@@ -0,0 +1,39 @@
+import { IErrorLog, MonitorReporterConfig } from '@web-guard/types';
+import { getUUIDFromLog } from '@web-guard/utils';
+
+export class MonitorReporter {
+  repetitionErrorRemove: boolean; // 是否进行错误上报去重
+  existLogUUIDs: Set<string>;
+
+  constructor(config: MonitorReporterConfig = {}) {
+    this.repetitionErrorRemove = config.repetitionErrorRemove || true;
+    this.existLogUUIDs = new Set();
+  }
+
+  bindConfig(options: MonitorReporterConfig): void {
+    if (options.repetitionErrorRemove !== undefined) {
+      this.repetitionErrorRemove = options.repetitionErrorRemove;
+    }
+  }
+
+  async send(log: IErrorLog): Promise<void> {
+    if (!this.repetitionErrorRemove) {
+      return this.immediateSend(log);
+    }
+    const uuid = getUUIDFromLog(log);
+    if (this.existLogUUIDs.has(uuid)) return;
+    this.existLogUUIDs.add(uuid);
+    return this.immediateSend(log);
+  }
+
+  async immediateSend(log: IErrorLog): Promise<void> {
+    console.log('log:', log)
+    // TODO 上报
+  }
+
+}
+
+const reporter = new MonitorReporter();
+export {
+  reporter,
+}
