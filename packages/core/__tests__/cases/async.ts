28dcf6cb5331249ee8f67c9da9b2643409647ebb
gamejoye <gamejoye@gmail.com>
2024-12-16 19:43:40 +0800

    test(core): 测试接口异常上报

---
+++ packages/core/__tests__/cases/async.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { WebGuard } from '@web-guard/core';
+export function setupAsyncTests(guard: WebGuard) {
+  const fetchBtn = document.getElementById('fetchBtn');
+  if (fetchBtn) {
+    fetchBtn.addEventListener('click', () => {
+      fetch('https://httpstat.us/404');
+    });
+  }
+  const fetchErrorBtn = document.getElementById('fetchErrorBtn');
+  if (fetchErrorBtn) {
+    fetchErrorBtn.addEventListener('click', () => {
+      fetch('https://www.baidu.com');
+    });
+  }
+  const xhrBtn = document.getElementById('xhrBtn');
+  if (xhrBtn) {
+    xhrBtn.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://httpstat.us/404');
+      xhr.send();
+    });
+  }
+
+  const xhrErrorBtn = document.getElementById('xhrErrorBtn');
+  if (xhrErrorBtn) {
+    xhrErrorBtn.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://www.baidu.com');
+      xhr.send();
+    });
+  }
+
+  const xhrTimeoutBtn = document.getElementById('xhrTimeoutBtn');
+  if (xhrTimeoutBtn) {
+    xhrTimeoutBtn.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://httpstat.us/200');
+      xhr.timeout = 500;
+      xhr.setRequestHeader('X-HttpStatus-Sleep', '2000'); // 延迟5秒响应
+      xhr.send();
+    });
+  }
+
+  const xhrCorrectBtn = document.getElementById('xhrCorrectBtn');
+  if (xhrCorrectBtn) {
+    xhrCorrectBtn.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://httpstat.us/200');
+      xhr.send();
+    });
+  }
+}
--- packages/core/__tests__/main.ts
+++ packages/core/__tests__/main.ts
@@ -3,12 +3,13 @@ import { setupErrorTests } from './cases/error';
 import { setupPromiseTests } from './cases/promise';
 import { setupBreadcrumbTests } from './cases/breadcrumb';
 import { setupSourceTests } from './cases/source';
-
+import { setupAsyncTests } from './cases/async';
 // 初始化各个测试用例
 setupErrorTests(guard);
 setupPromiseTests(guard);
 setupBreadcrumbTests(guard);
 setupSourceTests(guard);
+setupAsyncTests(guard);
 
 guard.init({
   targetUrl: 'testurl',
